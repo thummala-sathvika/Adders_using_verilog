7c8797ed5de48b6ef2ba23ac0f6a6cf187b6494d
Thummala Sathvika <68094844+thummala-sathvika@users.noreply.github.com>
2021-07-19 21:25:50 +0530

    Create full_adder.v

---
+++ full_adder/full_adder.v
@@ -0,0 +1,25 @@
+
+
+module full_adder(a,
+                  b,
+                  c,
+                  sum,
+                  carry);
+
+  // Step 1. Write down the directions for the ports	     
+input a,b,c;
+
+output sum,carry;
+  // Step 2. Declare the internal wires    
+wire w1,w2,w3;
+
+
+  // Step 3. Instantiate two Half-Adders
+
+half_adder HA1(.s(s),.b(b),.sum(sum),.carry(w2));
+half_adder HA2(.s(w1),.b(c),.sum(sum),.carry(w3));
+  // Step 4. Instantiate the OR gate
+or or1(carry,w2,w3);
+
+
+endmodule
