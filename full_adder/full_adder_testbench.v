2ba27454b61e116e8032fdddcdb3d51fa9eff623
Thummala Sathvika <68094844+thummala-sathvika@users.noreply.github.com>
2021-07-19 21:26:47 +0530

    Create full_adder_testbench.v

---
+++ full_adder/full_adder_testbench.v
@@ -0,0 +1,34 @@
+
+
+module full_adder_tb();
+
+  reg   a,b,cin;
+  wire  sum,carry;
+ 
+  integer i;
+
+  //Instantiate the full adder
+
+
+  initial 
+  begin
+    a   = 1'b0;
+    b   = 1'b0;
+    cin = 1'b0;
+  end
+
+  initial
+  begin 
+    for (i=0;i<8;i=i+1)
+    begin
+      {a,b,cin}=i;
+      #10;
+    end
+  end
+
+  initial $monitor("Input a=%b, b=%b, c=%b, Output sum =%b, carry=%b",
+                  a,b,cin,sum,carry);
+
+  initial #100 $finish;
+
+endmodule
